ed8f173a9a30b6c9e5bd9184345635ee98de6ae0
Marcus Rugger <marcusrugger@yahoo.com>
2013-12-06 21:40:02 -0500

    A quick and dirty C solution to problem 7

---
+++ problem-007/problem-7.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+
+bool is_prime(int number)
+{
+  int max_x = (int) sqrt((double) number);
+  
+  for (int x = 3; x <= max_x; x += 2)
+    if (number % x == 0)
+      return false;
+
+  return true;
+}
+
+
+int main(void)
+{
+  int prime_countdown = 9999;
+  int last_prime = 3;
+
+  while (prime_countdown > 0)
+  {
+    int prime_candidate;
+    for (prime_candidate = last_prime + 2; !is_prime(prime_candidate); prime_candidate += 2);
+    last_prime = prime_candidate;
+    prime_countdown--;
+  }
+
+  printf("%d\n", last_prime);
+  return 0;
+}
