904bd9dee8382de2bcde407733d9f2feb343609f
Marcus Rugger <marcusrugger@yahoo.com>
2013-12-08 08:46:23 -0500

    This time I really added the digit names

---
+++ problem-017/problem-17.rb
@@ -0,0 +1,23 @@
+
+$digit_names = [ "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ]
+
+$teen_names = [ "ten", "eleven", "twelve", "thirteen", "fourteen",
+                "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" ]
+
+$tens_names = [ "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" ]
+
+$hundred = "hundred"
+$thousand = "thousand"
+
+
+class Digits
+
+  def initialize
+  end
+
+end
+
+
+def append_digit_name(number)
+
+end
