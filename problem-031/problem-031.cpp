e057c4f26cface91029f663a03c8f4ab7e2891cd
Marcus Rugger <marcusrugger@yahoo.com>
2014-08-24 09:52:50 -0400

    Check point

--- problem-031/problem-031.cpp
+++ problem-031/problem-031.cpp
@@ -5,15 +5,26 @@ int main(int argc, char **argv)
 {
   const int TARGET_VALUE = 200;
   const int coin_values[] = { 200, 100, 50, 20, 10, 5, 2, 1, 0 };
+  int solutions_count = 0;
 
-  for (cv_outer_index = 0; coin_values[cv_outer_index]; cv_outer_index++)
+  for (int cv_outer_index = 0; coin_values[cv_outer_index]; cv_outer_index++)
   {
-    for (cv_inner_index = cv_outer_index; coin_values[cv_inner_index]; cv_inner_index++)
+    int summed_value = 0;
+
+    for (int cv_inner_index = cv_outer_index; summed_value < TARGET_VALUE; cv_inner_index++)
     {
-      int summed_value = coin_values[cv];
-      while (summed_value < TARGET_VALUE)
+      do
+      {
+        if (summed_value + coin_values[cv_inner_index] <= TARGET_VALUE)
+          summed_value += coin_values[cv_inner_index];
+        else
+          break;
+      }
+      while (true);
     }
+
+    solutions_count++;
   }
 
-  printf("Hello world!\n");
+  printf("Total number of solutions: %d\n", solutions_count);
 }
