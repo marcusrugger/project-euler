397d94597d140d51b7d936d4cf2db63b20a7569c
Marcus Rugger <marcusrugger@yahoo.com>
2014-08-24 10:56:54 -0400

    A little cleanup for problem 31

--- problem-031/problem-031.cpp
+++ problem-031/problem-031.cpp
@@ -3,19 +3,17 @@
 const int TARGET_VALUE = 200;
 const int coin_values[] = { 1, 2, 5, 10, 20, 50, 100, 200, 0 };
 
-int test_path(int cv_index, int sum)
+int evaluate_path(int cv_index, int sum)
 {
-  if (coin_values[cv_index] == 0 || sum + coin_values[cv_index] > 200)
+  if (coin_values[cv_index] == 0 || sum + coin_values[cv_index] > TARGET_VALUE)
     return 0;
-  else if (sum + coin_values[cv_index] == 200)
+  else if (sum + coin_values[cv_index] == TARGET_VALUE)
     return 1;
-
-  return test_path(cv_index, sum + coin_values[cv_index]) + test_path(cv_index + 1, sum);
+  else
+    return evaluate_path(cv_index, sum + coin_values[cv_index]) + evaluate_path(cv_index + 1, sum);
 }
 
 int main(int argc, char **argv)
 {
-  int solutions_count = test_path(0, 0);
-
-  printf("Total number of solutions: %d\n", solutions_count);
+  printf("Total number of solutions: %d\n", evaluate_path(0, 0));
 }
