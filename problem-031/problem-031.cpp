d861985c10e82475f6d5764c1d06a565ec6a0921
Marcus Rugger <marcusrugger@yahoo.com>
2014-08-24 10:35:20 -0400

    Solution for problem 31

--- problem-031/problem-031.cpp
+++ problem-031/problem-031.cpp
@@ -1,30 +1,21 @@
 #include <stdio.h>
 
+const int TARGET_VALUE = 200;
+const int coin_values[] = { 1, 2, 5, 10, 20, 50, 100, 200, 0 };
+
+int test_path(int cv_index, int sum)
+{
+  if (coin_values[cv_index] == 0 || sum + coin_values[cv_index] > 200)
+    return 0;
+  else if (sum + coin_values[cv_index] == 200)
+    return 1;
+
+  return test_path(cv_index, sum + coin_values[cv_index]) + test_path(cv_index + 1, sum);
+}
 
 int main(int argc, char **argv)
 {
-  const int TARGET_VALUE = 200;
-  const int coin_values[] = { 200, 100, 50, 20, 10, 5, 2, 1, 0 };
-  int solutions_count = 0;
-
-  for (int cv_outer_index = 0; coin_values[cv_outer_index]; cv_outer_index++)
-  {
-    int summed_value = 0;
-
-    for (int cv_inner_index = cv_outer_index; summed_value < TARGET_VALUE; cv_inner_index++)
-    {
-      do
-      {
-        if (summed_value + coin_values[cv_inner_index] <= TARGET_VALUE)
-          summed_value += coin_values[cv_inner_index];
-        else
-          break;
-      }
-      while (true);
-    }
-
-    solutions_count++;
-  }
+  int solutions_count = test_path(0, 0);
 
   printf("Total number of solutions: %d\n", solutions_count);
 }
