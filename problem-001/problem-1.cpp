cee55d9585395f3257951096d7f393f9d9b8665f
Marcus Rugger <marcusrugger@yahoo.com>
2014-10-07 10:42:02 -0400

    Added python solution for problem 1

--- problem-001/problem-1.cpp
+++ problem-001/problem-1.cpp
@@ -9,13 +9,19 @@ int main(void)
   for (int x = 3; x < END_OF_LOOP; x += 3)
     sum_threes += x;
 
+  printf("step 3: %d\n", sum_threes);
+
   int sum_fives = 0;
   for (int x = 5; x < END_OF_LOOP; x += 5)
     sum_fives += x;
 
+  printf("step 5: %d\n", sum_fives);
+
   int sum_intersection = 0;
   for (int x = 15; x < END_OF_LOOP; x += 15)
     sum_intersection += x;
+
+  printf("intersection: %d\n", sum_intersection);
 
   int total_sums = sum_threes + sum_fives - sum_intersection;
   printf("%d\n", total_sums);
--- problem-001/problem-1.go
+++ problem-001/problem-1.go
@@ -2,11 +2,18 @@ package main
 
 import "fmt"
 
+
+func doittoit(step int, end int) int {
+  var sum int = 0
+  for i := 0; i < end; i += step { sum += i }
+  return sum
+}
+
+
 func main() {
   const end = 1000
-  var sum3, sum5, sum15 int
-  for i := 0; i < end; i += 3 { sum3 += i }
-  for i := 0; i < end; i += 5 { sum5 += i }
-  for i := 0; i < end; i += 15 { sum15 += i }
+  var sum3 int = doittoit(3, end)
+  var sum5 int = doittoit(5, end)
+  var sum15 int = doittoit(15, end)
   fmt.Println(sum3, sum5, sum15, sum3 + sum5 - sum15)
 }
