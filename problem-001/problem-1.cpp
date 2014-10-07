15b4f5bf0297bab34f0434f0a84414b4dccce09e
Marcus Rugger <marcusrugger@yahoo.com>
2014-10-07 15:23:58 -0400

    Added a java solution to problem 1

--- problem-001/problem-1.cpp
+++ problem-001/problem-1.cpp
@@ -1,21 +1,18 @@
-#include <stdio.h>
+#include <iostream>
 
 const int END_OF_LOOP = 1000;
 
 
-int calc_sum(int step)
-{
-  int sum = 0;
-
-  for (int a = 0; a < END_OF_LOOP; a += step)
-    sum += a;
-
-  return sum;
-}
-
-
 int main(void)
 {
-  printf("%d\n", calc_sum(3) + calc_sum(5) - calc_sum(15));
+  auto calc_sum = [](int step)
+  {
+    int sum = 0;
+    for (int a = 0; a < END_OF_LOOP; a += step)
+      sum += a;
+    return sum;
+  };
+
+  std::cout << calc_sum(3) + calc_sum(5) - calc_sum(15) << std::endl;
   return 0;
 }
---
+++ problem-001/problem1.java
@@ -0,0 +1,15 @@
+
+public class problem1 {
+
+    public static int calc_sum(int step) {
+        int sum = 0;
+        for (int a = 0; a < 1000; a += step)
+            sum += a;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calc_sum(3) + calc_sum(5) - calc_sum(15));
+    }
+
+}
