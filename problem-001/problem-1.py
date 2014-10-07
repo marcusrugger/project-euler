cf5e491abffafe98eff7b89141ffcf73140c6837
Marcus Rugger <marcusrugger@yahoo.com>
2014-10-07 10:55:45 -0400

    Don't need my_range after all

--- problem-001/problem-1.py
+++ problem-001/problem-1.py
@@ -3,28 +3,22 @@
 END_OF_LOOP = 1000
 
 
-def my_range(start, stop, step):
-  while start < stop:
-    yield start
-    start += step
-
-
 step_3 = 0
-for num in my_range(3, END_OF_LOOP, 3):
+for num in range(3, END_OF_LOOP, 3):
   step_3 += num
 
 print step_3
 
 
 step_5 = 0
-for num in my_range(5, END_OF_LOOP, 5):
+for num in range(5, END_OF_LOOP, 5):
   step_5 += num
 
 print step_5
 
 
 step_15 = 0
-for num in my_range(15, END_OF_LOOP, 15):
+for num in range(15, END_OF_LOOP, 15):
   step_15 += num
 
 print step_15
