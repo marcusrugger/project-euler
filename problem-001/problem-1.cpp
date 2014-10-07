1459582af58bb9658cd1730dfdf828438b35c531
Marcus Rugger <marcusrugger@yahoo.com>
2014-10-07 13:20:43 -0400

    Simplified problem-001

--- problem-001/problem-1.cpp
+++ problem-001/problem-1.cpp
@@ -1,30 +1,21 @@
 #include <stdio.h>
 
+const int END_OF_LOOP = 1000;
+
+
+int calc_sum(int step)
+{
+  int sum = 0;
+
+  for (int a = 0; a < END_OF_LOOP; a += step)
+    sum += a;
+
+  return sum;
+}
+
 
 int main(void)
 {
-  const int END_OF_LOOP = 1000;
-
-  int sum_threes = 0;
-  for (int x = 3; x < END_OF_LOOP; x += 3)
-    sum_threes += x;
-
-  printf("step 3: %d\n", sum_threes);
-
-  int sum_fives = 0;
-  for (int x = 5; x < END_OF_LOOP; x += 5)
-    sum_fives += x;
-
-  printf("step 5: %d\n", sum_fives);
-
-  int sum_intersection = 0;
-  for (int x = 15; x < END_OF_LOOP; x += 15)
-    sum_intersection += x;
-
-  printf("intersection: %d\n", sum_intersection);
-
-  int total_sums = sum_threes + sum_fives - sum_intersection;
-  printf("%d\n", total_sums);
-
+  printf("%d\n", calc_sum(3) + calc_sum(5) - calc_sum(15));
   return 0;
 }
--- problem-001/problem-1.py
+++ problem-001/problem-1.py
@@ -3,26 +3,11 @@
 END_OF_LOOP = 1000
 
 
-step_3 = 0
-for num in range(3, END_OF_LOOP, 3):
-  step_3 += num
-
-print step_3
-
-
-step_5 = 0
-for num in range(5, END_OF_LOOP, 5):
-  step_5 += num
-
-print step_5
+def calc_sum(step):
+  sum = 0
+  for num in range(step, END_OF_LOOP, step):
+    sum += num
+  return sum
 
 
-step_15 = 0
-for num in range(15, END_OF_LOOP, 15):
-  step_15 += num
-
-print step_15
-
-
-solution = step_3 + step_5 - step_15
-print solution
+print calc_sum(3) + calc_sum(5) - calc_sum(15)
