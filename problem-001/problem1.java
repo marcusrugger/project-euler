a9fc4b64f464aed9fcb3a3d27359af09b62f1fd0
Marcus Rugger <marcusrugger@yahoo.com>
2014-10-08 13:24:37 -0400

    Updating java

--- problem-001/problem1.java
+++ problem-001/problem1.java
@@ -1,15 +1,16 @@
+package java8;
 
 public class problem1 {
 
-    public static int calc_sum(int step) {
-        int sum = 0;
-        for (int a = 0; a < 1000; a += step)
-            sum += a;
-        return sum;
-    }
+  public static int calc_sum(int step) {
+    int sum = 0;
+    for (int a = step; a < 1000; a += step)
+      sum += a;
+    return sum;
+  }
 
-    public static void main(String[] args) {
-        System.out.println(calc_sum(3) + calc_sum(5) - calc_sum(15));
-    }
+  public static void main(String[] args) {
+    System.out.println(calc_sum(3) + calc_sum(5) - calc_sum(15));
+  }
 
 }
